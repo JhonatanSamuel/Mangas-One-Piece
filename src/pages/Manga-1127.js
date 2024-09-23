ad25f4503b54584302b62f2c37f4ca9c3ed81119
JhonatanSamuel <jhonatsamuel@gmail.com>
2024-09-23 13:58:24 -0300

    Troca de img

--- src/pages/Manga-1127.js
+++ src/pages/Manga-1127.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Div, Img } from '../styles';
-import cap01 from './../assets/1127/1127 - 1.jpeg'
-import cap02 from './../assets/1127/1127 - 2.jpeg'
-import cap03 from './../assets/1127/1127 - 3.jpeg'
-import cap04 from './../assets/1127/1127 - 4.jpeg'
-import cap05 from './../assets/1127/1127 - 5.jpeg'
-import cap06 from './../assets/1127/1127 - 6.jpeg'
-import cap07 from './../assets/1127/1127 - 7.jpeg'
-import cap08 from './../assets/1127/1127 - 8.jpeg'
-import cap09 from './../assets/1127/1127 - 9.jpeg'
-import cap10 from './../assets/1127/1127 - 10.jpeg'
-import cap11 from './../assets/1127/1127 - 11.jpeg'
-import cap12 from './../assets/1127/1127 - 12.jpeg'
-import cap13 from './../assets/1127/1127 - 13.jpeg'
+import cap01 from './../assets/1127/1127 - 1-.jpeg'
+import cap02 from './../assets/1127/1127 - 2-.jpeg'
+import cap03 from './../assets/1127/1127 - 3-.jpeg'
+import cap04 from './../assets/1127/1127 - 4-.jpeg'
+import cap05 from './../assets/1127/1127 - 5-.jpeg'
+import cap06 from './../assets/1127/1127 - 6-.jpeg'
+import cap07 from './../assets/1127/1127 - 7-.jpeg'
+import cap08 from './../assets/1127/1127 - 8-.jpeg'
+import cap09 from './../assets/1127/1127 - 9-.jpeg'
+import cap10 from './../assets/1127/1127 - 10-.jpeg'
+import cap11 from './../assets/1127/1127 - 11-.jpeg'
+import cap12 from './../assets/1127/1127 - 12-.jpeg'
+import cap13 from './../assets/1127/1127 - 13-.jpeg'
 
 
 import { Link } from 'react-router-dom';
